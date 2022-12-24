a55c0e7db24e2eeac37a747df85f2bdf8f2aa678
Joako <joaquin.vannucci202875@alumnos.info.unlp.edu.ar>
2022-12-24 18:01:41 -0300

    Desafio clase 12

---
+++ JS/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const app = express();
+
+const mensajes = [];
+
+app.use(express.static('public'));
+
+io.on('connection', socket => {
+    console.log('Nuevo cliente conectado');
+    socket.emit('mensajes', mensajes)
+    socket.on('mensaje', data => {
+      mensajes.push({socketid: socket.id, mensaje: data})
+      io.sockets.emit('mensajes', mensajes)
+    });
+});
+
+const PORT = 8080
+const srv = server.listen(PORT, function() {
+  console.log('Servidor HTTP con WebSockets escuchando en el puerto: ', PORT);
+}) 
+srv.on('error', error => console.log('Error en servidor: ', error))
+
